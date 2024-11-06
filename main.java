4b66d5323d5407848d06e95a23c5e8ac665dc4b1
kazi-Obaid <kaziobaid2@gmail.com>
2024-11-07 01:16:33 +0600

    Update main.java
    Now the project concept start.

--- main.java
+++ main.java
@@ -1,5 +1,9 @@
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
-public class HealthAndDietScreen {
+public class HealthCalculator {
+
+    // Function to calculate BMI
+    public static double calculateBMI(double weightKg, double heightFeet) {
+        double heightMeters = heightFeet * 0.3048; // convert feet to meters
+        return weightKg / (heightMeters * heightMeters);
+    }
